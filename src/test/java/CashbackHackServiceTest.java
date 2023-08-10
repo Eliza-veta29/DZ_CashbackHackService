962caa932922a0497e96c2669dd8963f3023ae6d
Liza <skomorohova-elizaveta@rambler.ru>
2023-08-10 17:03:09 +0300

    testng

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.service;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
     public class CashbackHackServiceTest {
 
